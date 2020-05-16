9b00f251552e547a6fba6b48bec87f4a4e7c58ef
Arbidha <arbidha1412@gmail.com>
2020-05-16 17:39:35 -0500

    Added todo funtionality

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,32 @@
 import 'source-map-support/register'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { getUploadUrl, getAttachmentUrl } from '../../dataLayer/s3Access'
+import { updateAttachmentUrl } from '../../businessLogic/todos'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
   const todoId = event.pathParameters.todoId
+  const uploadUrl = getUploadUrl(todoId)
+  const attachmentUrl = getAttachmentUrl(todoId)
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
-}
+  await updateAttachmentUrl(todoId, attachmentUrl, jwtToken)
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      uploadUrl: uploadUrl
+    })
+  }
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
